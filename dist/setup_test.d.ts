c38f5bb0fb992092e8973262ebe2d712469ed80d
jcham <jaime.cham@cardinalblue.com>
2019-10-08 17:08:37 +0800

    removed dist, files in package.json

--- dist/setup_test.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="jest" />
-import { Elementable } from "./util";
-import { TestScheduler } from 'rxjs/testing';
-export declare function expectElem<T>(x: Elementable<T>): jest.Matchers<(T extends ArrayLike<any> ? never : T)[]>;
-export declare function testScheduler(): TestScheduler;
--- dist/util_react.d.ts
+++
@@ -1,7 +0,0 @@
-import { DependencyList, MutableRefObject } from "react";
-import { Queue } from "./util";
-import { BehaviorSubject, Observable } from "rxjs";
-export declare function useObservable<T>(observable: Observable<T>, defaultValue: T, inputs?: DependencyList): T;
-export declare function useBehaviorSubject<T>(subject: BehaviorSubject<T>): T;
-export declare function useObserving<T>(observable: Observable<T>, callback: (value: T) => void, inputs?: DependencyList): void;
-export declare function useFocusing(ref: MutableRefObject<HTMLElement | null>, doFocus: Queue<boolean>): void;
