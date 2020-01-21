8638ce4c51d0de761536b130b749ca1bac55c3d3
jcham <jaime.cham@cardinalblue.com>
2020-01-21 18:32:57 +0800

    fixed firestoreCloneCollection

--- src/firestore_clone.ts
+++ src/firestore_clone.ts
@@ -1,39 +1,44 @@
-import {CollectionRef, DocRef, DocSnap, firestoreSyncCollectionArray} from "./firestore_sync"
-import {Observable, throwError, zip} from "rxjs"
+import {CollectionRef, DocRef, DocSnap, firestoreSyncCollectionArray, QuerySnap} from "./firestore_sync"
+import {EMPTY, Observable, of, throwError, zip} from "rxjs"
 import {promise$} from "./util_rx"
-import {first, flatMap} from "rxjs/operators"
+import {first, flatMap, map} from "rxjs/operators"
 
 export function firestoreCloneReference(from: DocRef,
                                         toCollection: CollectionRef,
                                         extra: Object = {})
-    : Observable<DocRef>
+  : Observable<DocRef>
 {
-    return promise$(() => from.get()).pipe(
-        flatMap((docSnap: DocSnap) => firestoreCloneSnapshot(docSnap, toCollection, extra)),
-    )
+  return promise$(() => from.get()).pipe(
+    flatMap(docSnap => docSnap.exists ?
+      firestoreCloneSnapshot(docSnap, toCollection, extra)
+      : EMPTY),
+  )
 }
 export function firestoreCloneSnapshot(from: DocSnap,
                                        toCollection: CollectionRef,
                                        extra: Object = {})
-    : Observable<DocRef>
+  : Observable<DocRef>
 {
-    const data = from.data()
-    if (!data)
-        return throwError(`No data on docSnap ${from.id}`)
-    return promise$<DocRef>(() =>
-        toCollection.add({ ...data, ...extra }))
+  const data = from.data()
+  if (!data)
+    return throwError(`No data on docSnap ${from.id}`)
+  return promise$<DocRef>(() =>
+    toCollection.add({ ...data, ...extra }))
 
 }
 export function firestoreCloneCollection(fromCollection: CollectionRef,
-                                         toCollection: CollectionRef)
-    : Observable<DocRef[]>
+                                         toCollection: CollectionRef,
+                                         extra: Object = {})
+  : Observable<DocRef[]>
 {
-    return firestoreSyncCollectionArray(fromCollection).pipe(
-        first(),
-        flatMap(array => {
-            const eachSnap = (docSnap: DocSnap) => firestoreCloneSnapshot(docSnap, toCollection)
-            return zip(...array.map(eachSnap))
-        })
-    )
+  return promise$(() => fromCollection.get()).pipe(
+    flatMap((querySnap: QuerySnap) =>
+      querySnap.empty ? of([]) : of(querySnap.docs)
+    ),
+    flatMap(array => {
+      const eachSnap = (docSnap: DocSnap) => firestoreCloneSnapshot(docSnap, toCollection, extra)
+      return zip(...array.map(eachSnap))
+    })
+  )
 }
 
