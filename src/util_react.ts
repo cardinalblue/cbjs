4ebc5d719e2d5f0ec84e296c3b9042ab2b8710f1
jcham <jaime.cham@cardinalblue.com>
2020-04-28 15:03:57 +0800

    Added debugs to use*

--- src/util_react.ts
+++ src/util_react.ts
@@ -14,6 +14,8 @@ export function useObservable<T>(observable: Observable<T>,
 
   useEffect(() => {
     const subs = observable.subscribe((t: T) => {
+      if (useObservable.debug)
+        console.log("---- useObservable", t)
       setT(t)
     })
     return () => subs.unsubscribe()
@@ -21,12 +23,15 @@ export function useObservable<T>(observable: Observable<T>,
 
   return t
 }
+useObservable.debug = false
 
 export function useBehaviorSubject<T>(subject: BehaviorSubject<T>): T {
   const [t, setT] = useState(subject.value)
 
   useEffect(() => {
     const subs = subject.subscribe((t: T) => {
+      if (useBehaviorSubject.debug)
+        console.log("---- useBehaviorSubject", t)
       setT(t)
     })
     return () => subs.unsubscribe()
@@ -34,16 +39,22 @@ export function useBehaviorSubject<T>(subject: BehaviorSubject<T>): T {
 
   return t
 }
+useBehaviorSubject.debug = false
 
 export function useObserving<T>(observable: Observable<T>,
                                 callback: (value: T) => void,
                                 inputs: DependencyList = [observable])
   : void {
   useEffect(() => {
-    const subs = observable.subscribe(callback)
+    const subs = observable.subscribe(t => {
+      if (useObserving.debug)
+        console.log("---- useObserving", t)
+      callback(t)
+    })
     return () => subs.unsubscribe()
   }, [observable])
 }
+useObserving.debug = false
 
 // -----------------------------------------------------------------------
 // Focuses the given element whenever a `true` gets sent on the given Queue.
