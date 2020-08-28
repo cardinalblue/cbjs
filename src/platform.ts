6dc2b4c17d0128f5ec90a1e30713b91062b8b482
jasperhuangg <jasperhu@usc.edu>
2020-08-28 15:11:16 +0800

    added blobToFile

--- src/platform.ts
+++ src/platform.ts
@@ -41,3 +41,11 @@ const isTouchyF = function() {
   )
 }
 export const isTouchy = isTouchyF()
+
+export const blobToFile = (theBlob: Blob, fileName: string): File => {
+  const b: any = theBlob
+  // A Blob() is almost a File() - it's just missing the two properties below which we will add
+  b.lastModifiedDate = new Date()
+  b.name = fileName
+  return theBlob as File
+}
