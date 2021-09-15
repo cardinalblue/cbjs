6edfc8c45855a583737779dfd9ed801f31b69548
jcham <jaime.cham@cardinalblue.com>
2021-09-15 08:55:57 +0800

    Added optional id to firestoreModelCreate

--- src/util_firestore/firestore_model.ts
+++ src/util_firestore/firestore_model.ts
@@ -1,10 +1,11 @@
 import {concat, Observable, of, throwError} from "rxjs";
 import {map, mergeMap} from "rxjs/operators";
 import {CollectionRef, DocRef, firestoreDeleteCollection} from "./firestore_sync"
-import {addDoc, deleteDoc, doc, updateDoc} from "firebase/firestore"
+import {addDoc, deleteDoc, doc, setDoc, updateDoc} from "firebase/firestore"
 import {PartialExceptFor, SubsetBehaviorSubject$} from "../util_types"
 import {promise$} from "../util_rx"
 import firebase from "firebase/compat"
+import {ID} from "../util"
 import DocumentData = firebase.firestore.DocumentData
 
 
@@ -14,12 +15,20 @@ export type CreationRequiring<M, MRequired extends keyof SubsetBehaviorSubject$<
   PartialExceptFor<SubsetBehaviorSubject$<M>, MRequired>
 
 export function firestoreModelCreate<STRUCT>(collectionRef: CollectionRef,
-                                             struct: Partial<STRUCT>)
+                                             struct: Partial<STRUCT>,
+                                             id?: ID)
   : Observable<DocRef>
 {
-
-  return promise$(
-    () => addDoc(collectionRef, struct as DocumentData))
+  let write$: Observable<DocRef>
+  if (id === undefined)
+    write$ = promise$(() => addDoc(collectionRef, struct as DocumentData))
+  else {
+    const docRef = doc(collectionRef, id)
+    write$ = promise$(() => setDoc(docRef, struct as DocumentData)).pipe(
+      map(_ => docRef))
+  }
+  console.log("++++ firestoreModelCreate write$", write$)
+  return write$
     .pipe(
       mergeMap(docRef => {
         if (!docRef) {
