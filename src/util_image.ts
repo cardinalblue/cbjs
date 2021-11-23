19557853811f0e524d2526390c60573d8f07cde5
jcham <jaime.cham@cardinalblue.com>
2021-11-24 04:42:47 +0800

    added imageSize$

---
+++ src/util_image.ts
@@ -0,0 +1,19 @@
+import {Size} from "./kor"
+import {Observable} from "rxjs"
+
+function imageSize$(file: File): Observable<Size> {
+  return new Observable<Size>(subscriber => {
+    const url = URL.createObjectURL(file)
+    const image = new Image()
+    image.onload = () => {
+      subscriber.next(new Size(image.width, image.height))
+      subscriber.complete()
+    }
+    image.onerror = (e) =>
+      subscriber.error(e)
+    image.src = url
+
+    // ---- Cleanup
+    return () => URL.revokeObjectURL(url)
+  })
+}
